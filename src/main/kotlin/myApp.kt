453fa5127700818df69fdc1e6010c3a1221f7a74
bergjak <bergjak@amazon.com>
2022-07-08 11:41:50 -0400

    added a fibonacci function

--- src/main/kotlin/myApp.kt
+++ src/main/kotlin/myApp.kt
@@ -1,5 +1,22 @@
 fun main() {
-    val Fido = Dog("Fido")
-    Fido.bark()
-    println(Fido.name)
+    val fido = Dog("Fido")
+    fido.bark()
+
+    for (n in 1 until 10) {
+        println(fibonacci(n))
+    }
+
+}
+
+private fun fibonacci(n: Int): Int {
+    var (a, b, temp) = arrayOf(0, 1, 0)
+
+    for (i in 1 until n) {
+        // a, b = b, a + b
+        temp = a
+        a = b
+        b = a + temp
+    }
+
+    return b
 }
